847625517af230b097830efa49d77bd8fe4c8155
AlexanderTer <46087309+AlexanderTer@users.noreply.github.com>
2022-11-07 11:32:34 +0300

    Filter form1 form2

--- Inc/dsp.h
+++ Inc/dsp.h
@@ -1,15 +1,14 @@
 #ifndef  __DSP_H__
 #define __DSP_H__
 
-
-
 #define MAX_MOVING_FLOAT_SIZE 500
 #define MAX_MEDIAN_FLOAT_SIZE 500
+#define MAX_ORDER_DIGITAL_FILTER (2)
 
 #define TS (1/100.e3)    // Период дискретизации
 #define TAU_1ORD (0.001 / 2. / 3.1415926535) // Постоянная времени фильтра
 
-typedef struct{
+typedef struct {
 	float buf[MAX_MOVING_FLOAT_SIZE];
 
 	unsigned int pointer;
@@ -18,33 +17,42 @@ typedef struct{
 
 } MovingFloatFilter_Struct;
 
-
-typedef struct{
+typedef struct {
 	float buf[MAX_MEDIAN_FLOAT_SIZE];
 
 	float buf_sorted[MAX_MEDIAN_FLOAT_SIZE];
 
 	unsigned int pointer;
 
-
 } MedianFloatFilter_Struct;
 
-typedef struct{
+typedef struct {
 
 	float yn;
 
 	float b0;
 	float a1;
 
-}Low_Filter_1st_Order_Struct;
+} Low_Filter_1st_Order_Struct;
 
+typedef struct {
+	float a[MAX_ORDER_DIGITAL_FILTER + 1]; // КОэффициенты знаменателя ПФ фильтра
+	float b[MAX_ORDER_DIGITAL_FILTER + 1]; // Коэффициенты числителя ПФ фильтра
+
+	float z[2] [MAX_ORDER_DIGITAL_FILTER + 1]; // Массив для хранения предыдущих значений
+
+} DigitalFilter_Struct;
 
 float MovingFloatFilter(MovingFloatFilter_Struct *filter, float x);
 float MedianFloatFilter(MedianFloatFilter_Struct *filter, float x);
-float Low_Filter_1st_Order(Low_Filter_1st_Order_Struct * filter, float x);
+float Low_Filter_1st_Order(Low_Filter_1st_Order_Struct *filter, float x);
+float DirectFormII_FloatFilter(DigitalFilter_Struct * filter, float x);
+float DirectFormI_FloatFilter(DigitalFilter_Struct * filter, float x);
 
 extern MovingFloatFilter_Struct FILTER_MOV;
 extern MedianFloatFilter_Struct FILTER_MED;
 extern Low_Filter_1st_Order_Struct FILTER_1ORD;
+extern DigitalFilter_Struct FILTER_DIG1;
+extern DigitalFilter_Struct FILTER_DIG2;
 
 #endif
--- Src/dsp.c
+++ Src/dsp.c
@@ -19,6 +19,23 @@ Low_Filter_1st_Order_Struct FILTER_1ORD = {
 		.b0 = TS / (TAU_1ORD + TS),
 		.a1 = -TAU_1ORD / (TAU_1ORD + TS)
 };
+DigitalFilter_Struct FILTER_DIG1 = {
+		.b[0] = 0.013359200027857,
+		.b[1] = 0.026718400055713,
+		.b[2] = 0.013359200027857,
+		.a[0] =1,
+		.a[1] = -1.647459981076977,
+		.a[2] = 0.700896781188403,
+};
+
+DigitalFilter_Struct FILTER_DIG2 = {
+		.b[0] = 0.993755964953657,
+		.b[1] = -1.925108587845861,
+		.b[2] = 0.993755964953657,
+		.a[0] = 1,
+		.a[1] = -1.925108587845860,
+		.a[2] = 0.987511929907314,
+};
 
 float MovingFloatFilter(MovingFloatFilter_Struct *filter, float x) {
 
@@ -80,4 +97,56 @@ float Low_Filter_1st_Order(Low_Filter_1st_Order_Struct * filter, float x){
   filter->yn = y;
   return y;
 
+}
+
+/**
+ * \brief Функция цифрового фильтра в прямой форме II
+ * \param filter: структура с параметрами фильтра
+ * \param х: выходная переменная
+ * \return y: значение после фильтрации
+ */
+float DirectFormII_FloatFilter(DigitalFilter_Struct * filter, float x){
+	float y = 0;
+
+	// w = x
+	filter ->z[0][0] = x;
+	for (int i = MAX_ORDER_DIGITAL_FILTER; i >= 1; i--){
+
+		// Суммирование ветвей с коэффициентами а
+		filter -> z[0][0] -= filter ->z[0][i] * filter->a[i];
+
+		// Суммироание ветвей с коэффициентами b
+		y += filter->z[0][i] * filter ->b[i];
+
+		// Сохранение предыдущих значений
+		filter->z[0][i] = filter->z[0][i-1];
+
+
+	}
+
+	// y += w * b0
+	y += filter ->z[0][0] * filter ->b[0];
+
+	return y;
+}
+
+
+float DirectFormI_FloatFilter(DigitalFilter_Struct * filter, float x){
+	filter -> z[0][0] = x;
+	filter -> z[1][0] = x * filter->b[0];
+
+	// Вычисление первого каскада с коэффициентами b
+	for (int i = MAX_ORDER_DIGITAL_FILTER; i >= 1; i--){
+		// v = v + bi * x * z^(-i)
+		filter -> z[1][0] += filter ->z[0][i] * filter ->b[i];
+		filter-> z[0][i] = filter -> z[0][i-1];
+	}
+
+	// Вычисление второго каскада с коэффициентами а
+	for (int i = MAX_ORDER_DIGITAL_FILTER; i >= 1; i--){
+
+		filter -> z[1][0] -= filter ->z[1][i] * filter ->a[i];
+		filter-> z[1][i] = filter -> z[1][i-1];
+		}
+	return filter ->z[1][0];
 }
